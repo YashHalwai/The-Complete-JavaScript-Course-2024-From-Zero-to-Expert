7eed31c54fb334bf1600959091cec2f4d2cf6fa2
YashHalwai <halwaiyash45@gmail.com>
2024-02-28 12:56:28 +0530

    Section 3 Updated

--- 02_JavaScript_Fundamentals_Part_2/script.js
+++ 02_JavaScript_Fundamentals_Part_2/script.js
@@ -321,51 +321,51 @@ TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 
 
 ///////////////////////////////////////
 // Looping Backwards and Loops in Loops
-const jonas = [
-    'Jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Michael', 'Peter', 'Steven'],
-    true
-  ];
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven'],
+//     true
+//   ];
   
-  // 0, 1, ..., 4
-  // 4, 3, ..., 0
+//   // 0, 1, ..., 4
+//   // 4, 3, ..., 0
   
-  for (let i = jonas.length - 1; i >= 0; i--) {
-    console.log(i, jonas[i]);
-  }
+//   for (let i = jonas.length - 1; i >= 0; i--) {
+//     console.log(i, jonas[i]);
+//   }
   
-  for (let exercise = 1; exercise < 4; exercise++) {
-    console.log(`-------- Starting exercise ${exercise}`);
+//   for (let exercise = 1; exercise < 4; exercise++) {
+//     console.log(`-------- Starting exercise ${exercise}`);
   
-    for (let rep = 1; rep < 6; rep++) {
-      console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
-    }
-  }
+//     for (let rep = 1; rep < 6; rep++) {
+//       console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+//     }
+//   }
   
   
-  ///////////////////////////////////////
-  // The while Loop
-  for (let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
-  }
+//   ///////////////////////////////////////
+//   // The while Loop
+//   for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+//   }
   
-  let rep = 1;
-  while (rep <= 10) {
-    // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
-    rep++;
-  }
+//   let rep = 1;
+//   while (rep <= 10) {
+//     // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+//     rep++;
+//   }
   
-  let dice = Math.trunc(Math.random() * 6) + 1;
+//   let dice = Math.trunc(Math.random() * 6) + 1;
   
-  while (dice !== 6) {
-    console.log(`You rolled a ${dice}`);
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if (dice === 6) console.log('Loop is about to end...');
-  }
-  */
+//   while (dice !== 6) {
+//     console.log(`You rolled a ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6) console.log('Loop is about to end...');
+//   }
+
   
   ///////////////////////////////////////
   // Coding Challenge #4
